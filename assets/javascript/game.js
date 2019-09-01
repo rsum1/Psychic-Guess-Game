f6a1e3284a1a72112b96ed210b86a2edcfad5702
rsum1 <ryanjsumpter+github@gmail.com>
2019-08-31 18:59:14 -0700

    finished logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,4 @@
-// VARIABLES
-//========================
+//INITIALIZATION
 
 let guesses = 8
 let wins = 0
@@ -11,10 +10,10 @@ let letterPool = ["a", "b", "c", "d", "e", "f"]
 
 //select random word
 // var gameWord = wordPool[Math.floor(Math.random()*wordPool.length)];
-var gameLetter = letterPool[Math.floor(Math.random() * letterPool.length)];
+// var gameLetter = letterPool[Math.floor(Math.random() * letterPool.length)];
 
 // console.log(gameWord)
-console.log(gameLetter)
+// console.log(gameLetter)
 //========================
 
 
@@ -35,16 +34,27 @@ console.log(gameLetter)
 // MAIN PROCESS
 // ================================
 
-document.onkeyup = function (event) {
-    if (event.keyCode >= 65 && event.keyCode <= 90) {
+function Random() {
+    return letterPool[Math.floor(Math.random() * letterPool.length)]
+}
+gameLetter = Random()
+console.log(gameLetter)
 
-        let guess = event.key
+document.onkeyup = function (event) {
+
+    if (event.keyCode >= 65 && event.keyCode <= 90) {
+        var guess = event.key
+        console.log(guess)
 
         if (guess === gameLetter) {
             alert(`You Win`)
             wins++
             badGuess = 0
-
+            function Random() {
+                return letterPool[Math.floor(Math.random() * letterPool.length)]
+            }
+            gameLetter = Random()
+            console.log(`next letter : ${gameLetter}`)
         }
         else if (badGuess < 7) {
             badGuess++
@@ -54,27 +64,26 @@ document.onkeyup = function (event) {
             badGuess = 0
             alert('You Lose')
         }
-
-
-        console.log(guess)
-        console.log(`wins: ${wins}`)
-        console.log(`losses: ${losses}`)
-        console.log(`bad guesses: ${badGuess}`)
-
-
-        //Test if letter is part of word
-        // if (condition) {
-
-        // } else {
-
-        // }
-
-        //Display incorrect letter
-        // let badGuess = document.createElement('h1')
-        // badGuess.textContent = event.key
-        // document.getElementById("word").append(badGuess)
-
-
     }
+    // console.log(guess)
+    console.log(`wins: ${wins}`)
+    console.log(`losses: ${losses}`)
+    console.log(`bad guesses: ${badGuess}`)
 }
+
+
+
+    //Test if letter is part of word
+    // if (condition) {
+
+    // } else {
+
+    // }
+
+    //Display incorrect letter
+    // let badGuess = document.createElement('h1')
+    // badGuess.textContent = event.key
+    // document.getElementById("word").append(badGuess)
+
+
         // document.getElementById(`wins`).innerHTML(`${wins}`)
